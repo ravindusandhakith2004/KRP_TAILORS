0ee0891ca1c3884df56c6b002a826bd15893aa51
©Ravindu_Sandhalkith <ravindusandhakith19@gmail.com>
2025-11-21 14:48:37 +0530

    test1 by rls

---
+++ krp_tailors/src/main/java/com/example/krp_tailors/KrpTailorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.krp_tailors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrpTailorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrpTailorsApplication.class, args);
+    }
+
+}
---
+++ krp_tailors/src/test/java/com/example/krp_tailors/KrpTailorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.krp_tailors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrpTailorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
